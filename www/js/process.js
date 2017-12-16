23c9817046d22306ee3236486448d7989618cc3e
Mark Angelo Sanchez <30914042+markangelo24@users.noreply.github.com>
2017-12-15 22:55:32 -1200

    Add files via upload

---
+++ www/js/process.js
@@ -0,0 +1,150 @@
+let process = {
+	menu:function(){
+		let content = <div><center>
+			<a href='#' onClick={process.page1}><button>ROTC</button></a><br/><br/>
+			<a href='#' onClick={process.page2}><button>HISTORY</button></a><br/><br/>
+			<a href='#' onClick={process.page3}><button>SAMPLE IMAGES</button></a><br/><br/>
+			<a href='#' onClick={process.page4}><button>ROTC COMMANDS</button></a><br/><br/>
+		</center></div>;
+		ReactDOM.render(content,document.getElementById('root'));
+	},
+	page1:function(){
+		let content = <div><center><h3>
+				Reserve Officer Training Corps (ROTC) in the Philippines is one of three components<br/>
+				of the National Service Training Program, the civic education and defense preparedness <br/>
+				program for Filipino college students.ROTC aims to provide military education and<br/>
+				 training for students to mobilize them for national defense preparedness.<br/>
+				 Its specific objectives include preparation of college students for service in the <br/>
+				 Armed Forces of the Philippines in the event of an emergency and their training to become<br/>
+				  reservists and potential commissioned officers of the AFP.<br/><br/>
+
+
+				  Graduates of the ROTC advance program serve in all branches of the Armed Forces of the Philippines.<br/>
+				   In 2008, ROTC graduates of the officer candidate schools of the various services constituted<br/>
+				   roughly 75% of the AFP officer corps. The ROTC grants qualified student-cadets scholarship<br/>
+				    benefits through a merit-based incentive program in return for an obligation of military service<br/>
+				     in the reserve force, or active duty in the AFP if given the opportunity, after graduation.<br/><br/>
+
+				     ROTC student-cadets attend college like other students, but also receive basic military training and <br/>
+				     officer training from the branch of service that handles their schools ROTC unit. The students participate<br/>
+				      in regular ROTC instruction during the school year (one school year for Basic ROTC student-cadets and three<br/> 
+				      	school years for Advance ROTC cadet-officers), and extended training activities during the summer, such as<br/>
+				      	 the ROTC Summer Camp Training (RSCT) and the Advance ROTC Academic Phase Training (ARAPT).<br/>
+				      	  <img src="img/2.jpg"></img>
+				<br/><a href='#' onClick={process.menu}>back</a>
+			</h3></center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page2:function(){
+		let content = <div><center><h3>
+				ROTC in the Philippines began in 1912 when the Philippine Constabulary commenced<br/>
+				 with military instruction at the University of the Philippines. The university<br/>
+				  Board of Regents then made representations to the United States Department of War<br/>
+				   through the Governor-General and received the services of a United States Army officer<br/>
+				    who took on the duties of a professor of Military Science. Through this arrangement, the <br/>
+				    first official ROTC unit in the Philippines was established in the University of the<br/>
+				     Philippines on 3 July 1922.[6]<br/>
+<br/>
+The National University, Ateneo de Manila University, Liceo de Manila,<br/>
+ and Colegio de San Juan de Letran soon followed suit and organized their<br/>
+  own respective ROTC units. In 1936, the Office of the Superintendent for<br/>
+   ROTC Units under the Philippine Army was activated to supervise all ROTC units in the country.[6]<br/>
+   <img src="img/1.jpg"></img>
+				<br/><a href='#' onClick={process.menu}>back</a>
+			</h3></center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page3:function(){
+		let content = <div><center>
+			<h1>Some images of ROTC training</h1><br/>
+				<img src="img/1.jpg" height="300" width="400"></img><br/>
+				<img src="img/2.jpg" height="300" width="400"></img><br/>
+				<img src="img/3.jpg" height="300" width="400"></img><br/>
+				<img src="img/4.jpg" height="300" width="400"></img><br/>
+				<img src="img/5.jpg" height="300" width="400"></img><br/>
+				<img src="img/6.jpg" height="300" width="400"></img><br/>
+				<img src="img/7.jpg" height="300" width="400"></img><br/>
+				<img src="img/8.jpg" height="300" width="400"></img><br/>
+				<img src="img/9.jpg" height="300" width="400"></img><br/>
+				<br/><a href='#' onClick={process.menu}>back</a>
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page4:function(){
+	let content = <div><center><h1>
+"
+The Defense and Sporting Arms Show
+" from
+July 17-21,<br/>
+2008
+at the
+SM Megamall Megatrade Hall 1 and 2
+.<br/>
+
+Commands:<br/>
+	</h1>
+	<h3>
+	1.Prepatory and Command of execution<br/>
+	Ex. Harap sa kanan, harap! (harap sa kanan- prepatory) (harap!-execution)<br/>
+	2.Combined CommandsEx. Lumansag, tiwalag (parang lumansag pero di pa kayo dismissed, and di niyo<br/>
+		sasabaihin “ginoo salamat po ginoo” as compared to lumansag)<br/>
+	3.Abbreviated commands<br/>
+	Ex. KAD-LAKADPAW-LIPAW (FLIGHT)TO-HINTO<br/>
+	4. Directives- meaning informal yung commands. <br/>
+	Example: Lavarias, drop saharapan!<br/></h3>
+	<h1>
+STATIONARY COMMANDS
+	</h1>
+	<h3>
+1.
+TION (TUNE)/HARAP-
+alam niyo naman yan eh. Clenched fists feet together <br/>
+45 degrees. Tsaka walang nagsasalita ah. Diresto tingin. Wala rin tatawa<br/>
+<br/>
+2.Tikas Pahinga/Paluwag/Lipaw
+- tikas pahinga, extend niyo left foot niyo tasyung hands niyo nasa <br/>
+likod kita yung palms, right hand over left hand taposmay butas dapat <br/>
+yung form nila. just right above the belt line.. pag sinabingLIPAW.<br/>
+ I-tap niyo lang yung left hand niyo, yung parang ginawa natin lastweek, <br/>
+ pero hindi niyo isasara yung butas okay? Hahahaha. Tas PALUWAG,ibaba niyo<br/>
+  lang yung tikas pahinga below the belt line.<br/>
+BAWAL TIKASPAHINGA PAG NASA LIKOD OFFICER AH. =))<br/>
+<br/>
+3.TINDIG PALUWAG- parang naka PALUWAG pero yung ulo niyo nakatingin lang <br/>
+sa officer. Laging face the officer ha!<br/>
+<br/>
+4.HUMANDA HARAP-
+from either tikas pahinga or paluwag, mag tune langkayo pag kasabi ng HARAP.<br/>
+<br/>
+5.Pugay kamay, NA
+!- yung salute. Perpendicular yung arms sa shoulder, tasyung hands straight<br/>
+ dapat. Tip of middle finger sa tip ng right eyebrow.Kailangan di Makita yun <br/>
+ thumb and medyo tilted siya. And pag sinabingBABA KAMAY, NA. ibaba niyo niyo<br/>
+  after the command NA.<br/>
+  <br/>
+6.LUMASAG
+- dismissed na kayo niyan, sasabihin niyo “GINOO SALAMATPO GINOO”.<br/>
+ Then gawin niyo yung HARAP SA LIKOD HARAP.<br/>
+ <br/>
+7.ALIS/BALIK KUPYA- kupya meaning headgear. Di ko pa alam kungpaano<br/>
+ ito gawin, pero itatak niyo lang sa utak nyio na may ganitongcommand.<br/>
+<br/>
+	</h3>
+
+
+				<br/><a href='#' onClick={process.menu}>back</a>
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	loading:function(){
+		let content = <div><center><h1>
+					wait atleast 24 hours to load the  app............
+				</h1></center></div>;
+		ReactDOM.render(content,document.getElementById('root'));
+	}
+}
+
+process.loading();
+setTimeout(function(){
+	process.menu();
+},9000);
